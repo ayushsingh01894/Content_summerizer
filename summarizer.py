503fc7086fe86189a20471e97c5ad2a5f8c38a29
Ayush Singh <ayush.singh01894@gmail.com>
2026-07-04 02:04:58 +0530

    Adding something

--- summarizer.py
+++ summarizer.py
@@ -1,12 +1,13 @@
 from groq import Groq
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
+api_key = st.secrets.get("GROQ_API_KEY", os.getenv("GROQ_API_KEY"))
+
+client = Groq(api_key=api_key)
 
 # Summary Function
 def summarize_text(text, summary_type):
